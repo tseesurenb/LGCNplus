54f8876de42931da1976893dcb3430390222e31b
tseesurenb <133945855+tseesurenb@users.noreply.github.com>
2024-01-10 17:01:33 +1100

    add

--- model.py
+++ model.py
@@ -134,7 +134,7 @@ class LGCN(MessagePassing):
         
         if self.u_abs_drift:
             _u_abs_drift_emb = self._u_abs_drift_emb.weight[src]
-            _u_abs_drift_emb = _u_abs_drift_emb * u_abs_t_decay.unsqueeze(1) 
+            _u_abs_drift_emb = _u_abs_drift_emb * u_abs_t_decay.unsqueeze(1)
             _inner_pro = _inner_pro + _u_abs_drift_emb
             
         if self.u_rel_drift:
